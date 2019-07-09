a0de5d0071ed769687047ce19c358620e20f9a16
yang-i-yifei <qiannianhuashen@qq.com>
2019-07-09 09:29:20 +0800

    示意文件

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// 1. 导入组件
+
 
 export default {
   name: 'app',
+  // 2. 注册局部组件
   components: {
-    HelloWorld
+    
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
+// 程序的入口
+// 加载vue模块 CommonJS的模块化语法
+// const Vue = require('vue')
+
+// ES6中的模块化的语法，导入模块
 import Vue from 'vue'
+
+// 根组件
 import App from './App.vue'
 
+// 引入外部样式
+// import './xxx/index.css'
+
+// 提示当前处在开发模式, false 不提示
 Vue.config.productionTip = false
 
+// new Vue({
+//   el: '#app'
+// })
+
 new Vue({
-  render: h => h(App),
+  render: createElement => createElement(App),
 }).$mount('#app')
