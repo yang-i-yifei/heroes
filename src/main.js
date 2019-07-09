f11dfdd046b4391b8560b52ce4999db287a4cbef
yang-i-yifei <qiannianhuashen@qq.com>
2019-07-09 10:42:51 +0800

    配置路由

--- src/main.js
+++ src/main.js
@@ -9,15 +9,24 @@ import Vue from 'vue'
 import App from './App.vue'
 
 // 引入外部样式
-import './assets/css/index.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './assets/css/index.css'
+
 
 // 提示当前处在开发模式, false 不提示
 Vue.config.productionTip = false
+//导入vue-router
+import vueRouter from 'vue-router'
+//注册vuerouter插件 
+Vue.Vue.use(vueRouter)
+//创建路有对象
+const router = new vueRouter({
+  routes:[
 
-// new Vue({
-//   el: '#app'
-// })
+  ]
+})
+
+
 
 new Vue({
   render: createElement => createElement(App),
--- src/views/components/appSider.vue
+++ src/views/components/appSider.vue
@@ -1,26 +1,19 @@
 <template>
-<div>
+  <div>
     <div class="col-sm-3 col-md-2 sidebar">
-    <ul class="nav nav-sidebar">
-      <li class="active">
-        <a href="#">
-          Overview
-          <span class="sr-only">(current)</span>
-        </a>
-      </li>
-      <li>
-        <a href="#">Reports</a>
-      </li>
-      <li>
-        <a href="#">Analytics</a>
-      </li>
-      <li>
-        <a href="#">Export</a>
-      </li>
-    </ul>
+      <ul class="nav nav-sidebar">
+        <li class="active">
+          <a href="#">英雄列表</a>
+        </li>
+        <li>
+          <a href="#">武器列表</a>
+        </li>
+        <li>
+          <a href="#">装备列表</a>
+        </li>
+      </ul>
+    </div>
   </div>
-</div>
-  
 </template>
 
 <script>
